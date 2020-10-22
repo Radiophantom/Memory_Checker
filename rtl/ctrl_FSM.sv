cb37a614438e4e6768e8d0d2b4b93b4c0dc6b513
Sergey <sergey.serj.serj@gmail.com>
2020-10-22 15:53:57 +0300

    22.10

--- rtl/ctrl_FSM.sv
+++ rtl/ctrl_FSM.sv
@@ -1,10 +1,15 @@
 module ctrl_FSM #(
-
+  
 )(
+  input clk_i,
+  input rst_i,
 
+  input 
+
+  output 
 );
 
-typedef enum logic [2:0] { IDLE_S, START_TRANSACTION_S, } state, next_state;
+typedef enum logic [2:0] { IDLE_S, START_TEST_S, WRITE_ONLY_S, READ_ONLY_S, WRITE_WORD_S, READ_WORD_S, CHECK_WORD_S, END_TRANSACTION_S } state, next_state;
 
 logic [6:0] cmd_cnt;
 logic start_tr_en, compare_en, ;
@@ -26,50 +31,63 @@ always_comb
         end
       START_TEST_S :
         begin
-          case( csr[1][15:14] )
+          case( csr[1][17:16] )
             2'b00 : next_state = WRITE_ONLY_S;
             2'b01 : next_state = READ_ONLY_S;
-            2'b10 : next_state = WRITE_WORD_S;
-            2'b11 : next_state = WRITE_ONLY_S;
+            2'b10 : next_state = WRITE_ONE_WORD_S;
+            2'b11 : next_state = WRITE_ALL_WORDS_S;
           endcase
         end
       WRITE_ONLY_S :
         begin
-          if( cmd_cnt == 0 )
-            if( csr[1][15:14] == 2'b11 )
-              next_state = READ_ONLY_S;
-            else
-              next_state = END_TRANSACTION_S;
+          if( cmd_cnt_empty && cmd_accepted )
+            next_state = END_TRANSACTION_S;
         end
       READ_ONLY_S :
         begin
-          if( csr[1][15:14] == 2'b11 )
-            if( word_checked_sig && !correct_data_sig )
-              next_state = CHECK_DATA_FAILED_S;
-            else if( cmd_cnt == 0 )
-              next_state = END_TRANSACTION_S;
-          else if( cmd_cnt == 0 )
-           next_state = END_TRANSACTION_S;
+          if( cmd_cnt_empty && cmd_accepted )
+            next_state = END_TRANSACTION_S;
         end
-      WRITE_WORD_S :
+      WRITE_ONE_WORD_S :
         begin
           if( cmd_accepted )
-            next_state = READ_WORD_S;
+            next_state = READ_ONE_WORD_S;
         end
-      READ_WORD_S :
+      READ_ONE_WORD_S :
         begin
           if( cmd_accepted )
-            next_state = CHECK_WORD_S;
+            next_state = CHECK_ONE_WORD_S;
         end
-      CHECK_WORD_S :
+      CHECK_ONE_WORD_S :
         begin
           if( word_checked_sig )
             if( !correct_data_sig )
-              next_state = CHECK_DATA_FAILED_S;
-            else if( cmd_cnt != 0 )
-              next_state = WRITE_WORD_S;
-            else
+              next_state = ERROR_CHECK_WORD_S;
+            else if( cmd_cnt_empty )
               next_state = END_TRANSACTION_S;
+            else
+              next_state = WRITE_ONE_WORD_S;
+        end
+      WRITE_ALL_WORDS_S :
+        begin
+          if( cmd_cnt_empty && cmd_accepted )
+            next_state = RESTORE_CHECKER_STATE_S;
+        end
+      RESTORE_CHECKER_STATE_S :
+        begin
+          next_state = CHECK_ALL_WORDS_S;
+        end
+      CHECK_ALL_WORDS_S :
+        begin
+          if( word_checked_sig )
+            if( !correct_data_sig )
+              next_state = ERROR_CHECK_WORD_S;
+            else if( cmd_cnt_empty )
+              next_state = END_TRANSACTION_S;
+        end
+      ERROR_CHECK_WORD_S :
+        begin
+          next_state = IDLE_S;
         end
       END_TRANSACTION_S :
         begin
@@ -84,6 +102,43 @@ always_comb
 
 always_ff @( posedge clk_i, posedge rst_i )
   if( rst_i )
+    int_cmd_cnt <= '0;
+  else if( ( csr[1][15:13] == 3'b010 ) || ( csr[1][15:13] == 3'b011 ) )
+    if( state == START_TEST_S )
+      int_cmd_cnt <= ADDR_W;
+    else if( cmd_accepted )
+      if( int_cmd_cnt != 0 )
+        int_cmd_cnt <= int_cmd_cnt - 1;
+      else
+        int_cmd_cnt <= ADDR_W;
+
+always_ff @( posedge clk_i, posedge rst_i )
+  if( rst_i )
+    int_cmd_cnt_empty <= 1'b0;
+  else if( ( csr[1][15:13] != 3'b010 ) && ( csr[1][15:13] != 3'b011 ) )
+    if( state == START_TEST_S )
+      begin
+        int_cmd_cnt_empty <= 1'b1;
+      end
+  else
+    if( state == START_TEST_S )
+      begin
+        int_cmd_cnt_empty <= 1'b0;
+      end
+    else if( ( int_cmd_cnt == 1 ) && cmd_accepted )
+      int_cmd_empty <= 1'b1;
+    else
+      int_cmd_empty <= 1'b0;
+
+always_ff @( posedge clk_i, posedge rst_i )
+  if( rst_i )
+    save_checker_state <= 1'b0;
+  else
+
+
+
+always_ff @( posedge clk_i, posedge rst_i )
+  if( rst_i )
     cmp_en <= 1'b0;
   else if( ( state == READ_WORD_S ) && cmd_block_ready )
     cmp_en <= 1'b1;
@@ -93,21 +148,21 @@ always_ff @( posedge clk_i, posedge rst_i )
     cmp_en <= 1'b0;
 
 always_ff @( posedge clk_i, posedge rst_i )
-  if( rst_i )
-    restore_and_check <= 1'b0;
-  else if( ( state == WRITE_ONLY_S ) && ( cmd_cnt == 0 ) )
-    restore_and_check <= ( csr[1][15:14] == 2'b11 );
-  else
-    restore_and_check <= 1'b0;
-
-always_ff @( posedge clk_i, posedge rst_i )
   if( rst_i  )
     cmd_cnt <= 0;
-  else if( start_bit_set || ( restore_and_check == 1'b1 ) )
-    cmd_cnt <= csr[1][8:0];
-  else if( cmd_block_ready && ( cmd_cnt != 0 ) )
+  else if( start_bit_set || ( state == RESTORE_CHECKER_STATE_S ) )
+    cmd_cnt <= csr[1][31:20];
+  else if( cmd_accepted && ( cmd_cnt != 0 ) && int_cmd_empty )
     cmd_cnt <= cmd_cnt - 1;
 
+always_ff @( posedge clk_i, posedge rst_i )
+  if( rst_i )
+    cmd_cnt_empty <= '0;
+  else if(
+
+
+
+  
 always_ff @( posedge clk_i, posedge rst_i )
   if( rst_i )
     wr_en <= 1'b0;
@@ -153,14 +208,22 @@ always_ff @( posedge clk_i, posedge rst_i )
     end
   else
 
+assign cmp_data_en = ( state == READ_ONLY_S ) && ( !restore_and_check_sig ) && ( csr[1][15:14] == 2'b11 );
 
-always_ff @( posedge clk_i, posedge rst_i )
+// detect start-test bit set and reset start-test bit in csr after it
+always_ff @( posedge clk_2_i, posedge rst_i )
   if( rst_i )
     start_bit_sync_reg <= '0;
   else
     start_bit_sync_reg <= { start_bit_sync_reg[1:0], csr[0][0] };
 
-assign start_bit_set = ( start_bit_sync_reg[2] == 1'b0 ) && ( start_bit_sync_reg[1] == 1'b1 );
-assign cmp_data_en = ( state == READ_ONLY_S ) && ( !restore_and_check_sig ) && ( csr[1][15:14] == 2'b11 );
+always_ff @( posedge clk_1_i, posedge rst_i )
+  if( rst_i )
+    rst_start_bit_sync_reg <= '0;
+  else
+    rst_start_bit_sync_reg <= { rst_start_bit_sync_reg[1:0], start_bit_sync_reg[1] };
+
+assign start_bit_set = ( start_bit_sync_reg[2] == 1'b0     ) && ( start_bit_sync_reg[1] == 1'b1     );
+assign rst_start_bit = ( rst_start_bit_sync_reg[2] == 1'b0 ) && ( rst_start_bit_sync_reg[1] == 1'b1 );
 
 endmodule
